855d652949d4f92a87c748efbac70ba318ff3083
Serhat <serhatkochan@hotmail.com.tr>
2023-07-22 22:54:32 +0300

    init

--- App.js
+++ App.js
@@ -1,20 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import {SafeAreaView, View, Text, Button} from "react-native";
 
-export default function App() {
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+
+import Ionicons from "@expo/vector-icons/Ionicons"
+
+const Profile = ({navigation}) => {
+  const [user, setUser] = useState("Serhat");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View>
+        <Text>You have to sign in</Text>
+        <Button
+            title="SignIn"
+            onPress={() => navigation.navigate("SignIn", {user})}
+        />
+        <Button
+            title="SignUp"
+            onPress={() => navigation.navigate("SignUp")}
+        />
+      </View>
+  )
+}
+const SignIn = ({route}) => {
+  return <Text>Sign In {route.params.user}</Text>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const SignUp = () => {
+  return <Text>Sign Up</Text>
+}
+
+const ProfileStack = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Profile"
+                component={Profile}
+            />
+            <Stack.Screen
+                name="SignIn" component={SignIn}
+                options={({route}) => ({title: `gelen user parametresi: ${route.params.user}`})}
+            />
+            <Stack.Screen
+                name="SignUp"
+                component={SignUp}
+                options={{title: 'Sign UP'}}
+            />
+        </Stack.Navigator>
+    )
+}
+
+const Home = () => {
+    return <Text>Home</Text>
+}
+
+const MainTabs = () => {
+    return <Tabs.Navigator>
+        <Tabs.Screen
+            name="Home"
+            component={Home}
+            options={{
+                tabBarIcon: (props) => <Ionicons name="ios-home" {...props} />
+            }}
+        />
+        <Tabs.Screen
+            name="ProfileStack"
+            component={ProfileStack}
+            options={{
+                headerShown: false,
+                title: "Profile", //headerShown false oldugu icin zaten bu gozukmeyecek
+                tabBarIcon: (props) => <Ionicons name="ios-person" {...props} />
+
+            }}
+        />
+    </Tabs.Navigator>
+}
+
+const About = () => {
+    return <Text>About</Text>
+}
+
+const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator>
+                <Drawer.Screen name="MainTabs" component={MainTabs} />
+                <Drawer.Screen name="About" component={About} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default App;
